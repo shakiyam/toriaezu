325624a19186bcdce960613deba6a6b0343e4514
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2017-02-25 16:05:59 +0900

    Add install Make

--- provision.sh
+++ provision.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+set -eu -o pipefail
+
+# shellcheck disable=SC1091
+os_id=$(. /etc/os-release; echo "$ID")
+
+# Install Make
+case $os_id in
+  ol | amzn)
+    yum -y install make
+    ;;
+  ubuntu)
+    apt update
+    apt -y install make
+    ;;
+esac
 
 make toriaezu
 
