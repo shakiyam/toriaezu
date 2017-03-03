fd4162044a05a903a1737f82a71d34eeae8a5bd0
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2017-03-03 14:37:56 +0900

    Support Docker CE

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -7,50 +7,36 @@ os_id=$(. /etc/os-release; echo "$ID")
 os_version=$(. /etc/os-release; echo "$VERSION")
 
 # Install Docker Engine
-if [ "${1:-''}" = '--latest' ]; then
-  case $os_id in
-    ol)
-      yum -y install yum-utils
-      yum-config-manager --add-repo \
-        https://docs.docker.com/engine/installation/linux/repo_files/oracle/docker-ol7.repo
-      yum makecache fast
-      yum -y install docker-engine
-      ;;
-    amzn)
-      yum -y install docker
-      ;;
-    ubuntu)
+case $os_id in
+  ol)
+    case ${os_version%%.*} in
+      6)
+        repo=$(grep -E -o '(public_)?ol6_addons' /etc/yum.repos.d/public-yum-ol6.repo)
+        yum -y --enablerepo="$repo" install docker-engine
+        ;;
+      7)
+        yum -y --enablerepo=ol7_addons install docker-engine
+        ;;
+    esac
+    ;;
+  amzn)
+    yum -y install docker
+    ;;
+  ubuntu)
+    if [ "${1:-''}" = '--latest' ]; then
       apt-get install -y --no-install-recommends \
         apt-transport-https ca-certificates curl software-properties-common
-      curl -fsSL https://apt.dockerproject.org/gpg | apt-key add -
-      add-apt-repository \
-        "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+      sudo add-apt-repository \
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
       apt-get update
-      apt-get -y install docker-engine
-      ;;
-  esac
-else
-  case $os_id in
-    ol)
-      case ${os_version%%.*} in
-        6)
-          repo=$(grep -E -o '(public_)?ol6_addons' /etc/yum.repos.d/public-yum-ol6.repo)
-          yum -y --enablerepo="$repo" install docker-engine
-          ;;
-        7)
-          yum -y --enablerepo=ol7_addons install docker-engine
-          ;;
-      esac
-      ;;
-    amzn)
-      yum -y install docker
-      ;;
-    ubuntu)
+      apt-get -y install docker-ce
+    else
       apt-get update
       apt-get install -y docker.io
-      ;;
-  esac
-fi
+    fi
+    ;;
+esac
 
 # Setup Docker Engine
 usermod -aG docker "$(logname)"
