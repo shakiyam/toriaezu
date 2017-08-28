bcec7ad9395a41d804119224eb9fea7fbc6f0891
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2017-08-28 17:08:38 +0900

    Remove old docker

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -23,19 +23,14 @@ case $os_id in
     sudo yum -y install docker
     ;;
   ubuntu)
-    if [ "${1:-''}" = '--latest' ]; then
-      sudo apt update
-      sudo apt -y install --no-install-recommends \
-        apt-transport-https ca-certificates curl software-properties-common
-      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-      sudo add-apt-repository \
-        "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-      sudo apt update
-      sudo apt -y install docker-ce
-    else
-      sudo apt update
-      sudo apt -y install docker.io
-    fi
+    sudo apt update
+    sudo apt -y install --no-install-recommends \
+      apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+      "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt update
+    sudo apt -y install docker-ce
     ;;
 esac
 
