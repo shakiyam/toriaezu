739be6b1ebccd2451d82a936c22f326b7bd8ec18
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2017-09-14 12:24:18 +0900

    Fix install_dotfiles to run on Bash on Windows

--- scripts/install_dotfiles.sh
+++ scripts/install_dotfiles.sh
@@ -3,10 +3,11 @@ set -eu -o pipefail
 shopt -s dotglob
 
 dotfiles_dir=$(cd "$(dirname "$0")/../dotfiles" && pwd)
+logname=$(logname 2> /dev/null || id -nu)
 
 echo 'Copy dotfiles'
 for file in $(cd "$dotfiles_dir" && echo .[!.]*)
 do
-  cp -v "$dotfiles_dir/$file" "/home/$(logname)/$file"
-  chown "$(logname)":"$(id -g "$(logname)")" "/home/$(logname)/$file"
+  cp -v "$dotfiles_dir/$file" "/home/$logname/$file"
+  chown "$logname":"$(id -g "$logname")" "/home/$logname/$file"
 done
