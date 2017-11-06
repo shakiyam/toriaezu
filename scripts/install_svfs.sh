6247b47071c4e77f526297871b90b5b17fc53362
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2017-11-06 16:57:39 +0900

    Add SVFS

---
+++ scripts/install_svfs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -eu -o pipefail
+
+if [ -e .env ]; then
+  # shellcheck disable=SC1091
+  . .env
+fi
+
+# shellcheck disable=SC1091
+os_id=$(. /etc/os-release; echo "$ID")
+
+echo 'Install svfs'
+# Check the latest version from https://github.com/ovh/svfs/releases
+case $os_id in
+  ol | amzn)
+    sudo yum -y install ruby fuse
+    sudo rpm -ivh https://github.com/ovh/svfs/releases/download/v0.9.1/svfs-0.9.1-1.x86_64.rpm
+    ;;
+  ubuntu)
+    sudo apt update
+    sudo apt -y install ruby
+    FILE=$(mktemp)
+    curl -L# https://github.com/ovh/svfs/releases/download/v0.9.1/svfs_0.9.1_amd64.deb -o "$FILE"
+    sudo dpkg -i "$FILE"
+    rm "$FILE"
+    ;;
+esac
+
+echo 'Mount by svfs'
+cat <<EOT | sudo tee /etc/svfs.yaml >/dev/null
+os_auth_url: ${SVFS_AUTH_URL}
+os_username: ${SVFS_USERNAME}
+os_password: ${SVFS_PASSWORD}
+EOT
+sudo chmod 640 /etc/svfs.yaml
+sudo mkdir -p "${SVFS_MOUNT_POINT}"
+sudo mount -t svfs -o rw,allow_other,uid="$(id -u)",gid="$(id -g)" svfs "${SVFS_MOUNT_POINT}"
