87bbec0a82269b3bd0757160aa0d191a5bc5b505
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2017-12-06 18:17:58 +0900

    Adapt to new hadolint site

--- scripts/install_hadolint.sh
+++ scripts/install_hadolint.sh
@@ -11,13 +11,12 @@ if [ "$os_id" = 'ol' ] && [ "${os_version%%.*}" = '6' ] ; then
   sh "$(cd "$(dirname "$0")/.." && pwd)/bin/hadolint" -v
   sudo cp "$(cd "$(dirname "$0")/.." && pwd)/bin/hadolint" /usr/local/bin/hadolint
 else
-  # hadolint_latest=$(
-  #   curl -sSI https://github.com/lukasmartinelli/hadolint/releases/latest |
-  #     tr -d '\r' |
-  #     awk -F'/' '/^Location:/{print $NF}'
-  # )
-  hadolint_latest=v1.2.1
-  curl -L# "https://github.com/lukasmartinelli/hadolint/releases/download/${hadolint_latest}/hadolint_linux_amd64" |
+  hadolint_latest=$(
+    curl -sSI https://github.com/hadolint/hadolint/releases/latest |
+      tr -d '\r' |
+      awk -F'/' '/^Location:/{print $NF}'
+  )
+  curl -L# "https://github.com/hadolint/hadolint/releases/download/${hadolint_latest}/hadolint-Linux-x86_64" |
     sudo tee /usr/local/bin/hadolint >/dev/null
 fi
 sudo chmod +x /usr/local/bin/hadolint
