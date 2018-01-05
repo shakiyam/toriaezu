ad421f9ff6d3c93d09c6c30e64ca978f19d1b830
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2018-01-05 09:13:17 +0900

    Upgrade q

--- scripts/install_q.sh
+++ scripts/install_q.sh
@@ -8,13 +8,13 @@ echo 'Install Q'
 # Check the latest version from http://harelba.github.io/q/install.html
 case $os_id in
   ol | amzn)
-    sudo rpm -ivh https://github.com/harelba/packages-for-q/raw/master/rpms/q-text-as-data-1.6.3-1.noarch.rpm
+    sudo rpm -ivh https://github.com/harelba/packages-for-q/raw/master/rpms/q-text-as-data-1.7.1-1.noarch.rpm
     ;;
   ubuntu)
     sudo apt update
     sudo apt -y install python-minimal
     FILE=$(mktemp)
-    curl -L# https://github.com/harelba/packages-for-q/raw/master/deb/q-text-as-data_1.6.3-2_all.deb -o "$FILE"
+    curl -L# https://github.com/harelba/packages-for-q/raw/master/deb/q-text-as-data_1.7.1-2_all.deb -o "$FILE"
     sudo dpkg -i "$FILE"
     rm "$FILE"
     ;;
