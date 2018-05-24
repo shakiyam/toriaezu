84e1fcd26cb74fc59bc9645aab807a840fe6656a
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2018-05-24 15:39:57 +0900

    Use EPEL repos for Oracle LInux 6

--- scripts/install_bash-completion.sh
+++ scripts/install_bash-completion.sh
@@ -3,10 +3,20 @@ set -eu -o pipefail
 
 # shellcheck disable=SC1091
 os_id=$(. /etc/os-release; echo "$ID")
+# shellcheck disable=SC1091
+os_version=$(. /etc/os-release; echo "$VERSION")
 
 echo 'Install bash-completion'
 case $os_id in
-  ol | amzn)
+  ol)
+    case ${os_version%%.*} in
+      6)
+        sudo yum -y localinstall https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+        ;;
+    esac
+    sudo yum -y install bash-completion
+    ;;
+  amzn)
     sudo yum -y install bash-completion
     ;;
   ubuntu)
