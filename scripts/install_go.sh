0bf9577502a6a7ad47bd31417200f9a0b7a7f14e
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2018-08-31 21:21:28 +0900

    Update Go version

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -3,5 +3,5 @@ set -eu -o pipefail
 
 echo 'Install Go Programming Language'
 # Check the latest version from https://golang.org/dl/
-curl -L# https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz |
+curl -L# https://dl.google.com/go/go1.11.linux-amd64.tar.gz |
   sudo tar xzf - -C /usr/local
