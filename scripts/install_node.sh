a36b03e54640ba6354f18ce0097e11711bc64c83
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2019-01-21 15:40:03 +0900

    Upgrade Node.js and add Yarn

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -5,13 +5,19 @@ set -eu -o pipefail
 os_id=$(. /etc/os-release; echo "$ID")
 
 echo 'Install Node.js'
+# Check the latest version from https://nodejs.org/en/ and https://github.com/nodesource/distributions
 case $os_id in
   ol)
-    curl -sL https://rpm.nodesource.com/setup_8.x | sudo bash -
+    curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
     sudo yum -y install nodejs gcc-c++ make
+    curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo >/dev/null
+    sudo yum -y install yarn
     ;;
   ubuntu)
-    curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
     sudo apt -y install nodejs build-essential
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt -y install yarn
     ;;
 esac
