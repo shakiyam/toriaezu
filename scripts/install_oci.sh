92ffde3fa6b45cd5fe297eeea4e8ad958a703aa3
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2019-01-21 15:52:12 +0900

    Install dependents for Ubuntu

--- scripts/install_oci.sh
+++ scripts/install_oci.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -eu -o pipefail
 
+# shellcheck disable=SC1091
+os_id=$(. /etc/os-release; echo "$ID")
+
 echo 'Install OCI CLI'
+case $os_id in
+  ol)
+    ;;
+  ubuntu)
+    sudo apt -y install python3-distutils
+    ;;
+esac
 bash -c "$(curl -L# https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh)"
