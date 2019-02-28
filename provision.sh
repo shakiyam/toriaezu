6d92ad111a90cc4260a1edd04f27475b663133e4
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2019-02-28 14:02:27 +0900

    Fix for Oracle Linux 6

--- provision.sh
+++ provision.sh
@@ -3,12 +3,19 @@ set -eu -o pipefail
 
 # shellcheck disable=SC1091
 readonly OS_ID=$(. /etc/os-release; echo "$ID")
+# shellcheck disable=SC1091
+readonly OS_VERSION=$(. /etc/os-release; echo "$VERSION")
 
 # Install Make
 case $OS_ID in
   ol)
     sudo yum -y install make
-    [[ -e /usr/bin/ol_yum_configure.sh ]] && sudo /usr/bin/ol_yum_configure.sh && sudo yum install oracle-epel-release-el7.x86_64
+    if [[ -e /usr/bin/ol_yum_configure.sh ]]; then
+      sudo /usr/bin/ol_yum_configure.sh
+      if [[ "${OS_VERSION%%.*}" -eq 7 ]]; then
+        sudo yum install oracle-epel-release-el7.x86_64
+      fi
+    fi
     ;;
   ubuntu)
     sudo apt update
