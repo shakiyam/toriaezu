bb0ab7ebf068a3f0d968bf737709bb827ba4fe3c
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2019-02-28 14:42:04 +0900

    Fix for Oracle Linux 6

--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -3,11 +3,20 @@ set -eu -o pipefail
 
 # shellcheck disable=SC1091
 readonly OS_ID=$(. /etc/os-release; echo "$ID")
+# shellcheck disable=SC1091
+readonly OS_VERSION=$(. /etc/os-release; echo "$VERSION")
 
 echo 'Install OpenJDK Development Kit'
 case $OS_ID in
   ol)
-    sudo yum -y install java-11-openjdk-devel
+    case ${OS_VERSION%%.*} in
+      6)
+        sudo yum -y install java-1.8.0-openjdk-devel
+        ;;
+      7)
+        sudo yum -y install java-11-openjdk-devel
+        ;;
+    esac
     ;;
   ubuntu)
     sudo apt update
