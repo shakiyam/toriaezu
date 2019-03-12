1ecab349c6d626aaf7d7aaafea77c33672085147
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2019-03-12 16:00:00 +0900

    Upgrade Go

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -3,5 +3,5 @@ set -eu -o pipefail
 
 echo 'Install Go Programming Language'
 # Check the latest version from https://golang.org/dl/
-curl -L# https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz \
+curl -L# https://dl.google.com/go/go1.12.linux-amd64.tar.gz \
   | sudo tar xzf - -C /usr/local
