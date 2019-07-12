ff3b7ee7235438907802d940bb7fc1f232362378
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2019-07-12 17:45:59 +0900

    Fix misuse of SCL

--- scripts/install_python3.sh
+++ scripts/install_python3.sh
@@ -14,20 +14,17 @@ case $OS_ID in
         curl -ssL http://yum.oracle.com/public-yum-ol6.repo \
           | sudo tee /etc/yum.repos.d/public-yum-ol6.repo >/dev/null
         sudo yum -y --enablerepo=ol6_software_collections install rh-python36
-        set +u
-        # shellcheck disable=SC1091
-        . scl_source enable rh-python36
+        scl enable rh-python36 'python3 -m venv "$HOME/python3"'
         ;;
       7)
         sudo yum -y --enablerepo=ol7_developer_EPEL install python36
+        python3 -m venv "$HOME/python3"
         ;;
     esac
     ;;
   ubuntu)
     sudo apt update
     sudo apt -y install python3-venv
+    python3 -m venv "$HOME/python3"
     ;;
 esac
-python3 -m venv "$HOME/python3"
-# shellcheck disable=SC1090
-. "$HOME/python3/bin/activate"
