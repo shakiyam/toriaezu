eba07d366885e83bc1835836a67e5c17097be508
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2019-12-17 18:31:34 +0900

    Conform to shell style guide

--- scripts/install_checkstyle.sh
+++ scripts/install_checkstyle.sh
@@ -2,13 +2,13 @@
 set -eu -o pipefail
 
 echo 'Install Checkstyle'
-latest=$(
+readonly LATEST=$(
   curl -sSI https://github.com/checkstyle/checkstyle/releases/latest \
     | tr -d '\r' \
     | awk -F'/' '/^Location:/{print $NF}'
 )
 sudo mkdir -p /usr/local/lib/checkstyle
-curl -L# "https://github.com/checkstyle/checkstyle/releases/download/${latest}/${latest}-all.jar" \
+curl -L# "https://github.com/checkstyle/checkstyle/releases/download/${LATEST}/${LATEST}-all.jar" \
   | sudo tee /usr/local/lib/checkstyle/checkstyle.jar >/dev/null
 sudo cp "$(dirname "$0")/../bin/checkstyle" /usr/local/bin/checkstyle
 sudo chmod +x /usr/local/bin/checkstyle
