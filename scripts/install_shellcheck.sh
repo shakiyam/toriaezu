9dcec86d450525a3c96abb13ff7203b18ee5af4a
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2020-02-26 19:48:15 +0900

    Use pre-compiled binarie instead of docker images

--- scripts/install_shellcheck.sh
+++ scripts/install_shellcheck.sh
@@ -7,8 +7,10 @@ readonly OS_ID=$(. /etc/os-release; echo "$ID")
 echo 'Install ShellCheck'
 case $OS_ID in
   ol)
-    sudo bash "$(cd "$(dirname "$0")/.." && pwd)/bin/shellcheck" -V
-    sudo cp "$(cd "$(dirname "$0")/.." && pwd)/bin/shellcheck" /usr/local/bin/shellcheck
+    curl -L# https://storage.googleapis.com/shellcheck/shellcheck-stable.linux.x86_64.tar.xz \
+      | xz -d -c \
+      | sudo tar xf - -C /usr/local/bin/ --strip=1 shellcheck-stable/shellcheck
+    sudo chown root:root /usr/local/bin/shellcheck
     sudo chmod +x /usr/local/bin/shellcheck
     ;;
   ubuntu)
