884d5e3c8e391643fceaa5969c40abd986cae00d
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2020-05-23 19:42:24 +0900

    Support q 2.0.2 or higher

--- scripts/install_q.sh
+++ scripts/install_q.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 set -eu -o pipefail
 
-# shellcheck disable=SC1091
-readonly OS_ID=$(. /etc/os-release; echo "$ID")
-
 echo 'Install Q'
-# Check the latest version from http://harelba.github.io/q/install.html
-case $OS_ID in
-  ol)
-    sudo yum -y localinstall https://github.com/harelba/packages-for-q/raw/master/rpms/q-text-as-data-1.7.1-1.noarch.rpm
-    ;;
-  ubuntu)
-    sudo apt update
-    sudo apt -y install python-minimal
-    FILE=$(mktemp)
-    curl -L# https://github.com/harelba/packages-for-q/raw/master/deb/q-text-as-data_1.7.1-2_all.deb -o "$FILE"
-    sudo dpkg -i "$FILE"
-    rm "$FILE"
-    ;;
-esac
-sudo sed -i -e 1s/python$/python2/ "$(command -v q)"
+readonly LATEST=$(
+  curl -sSI https://github.com/harelba/q/releases/latest \
+    | tr -d '\r' \
+    | awk -F'/' '/^location:/{print $NF}'
+)
+curl -L# "https://github.com/harelba/q/releases/download/${LATEST}/q-x86_64-Linux" \
+  | sudo tee /usr/local/bin/q >/dev/null
+sudo chmod +x /usr/local/bin/q
