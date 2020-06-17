c8e226bda7320990ce0471025b56440224195a62
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2020-06-17 09:37:50 +0000

    Update to the new URL

--- scripts/install_shellcheck.sh
+++ scripts/install_shellcheck.sh
@@ -7,7 +7,7 @@ readonly OS_ID=$(. /etc/os-release; echo "$ID")
 echo 'Install ShellCheck'
 case $OS_ID in
   ol)
-    curl -L# https://storage.googleapis.com/shellcheck/shellcheck-stable.linux.x86_64.tar.xz \
+    curl -L# https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz \
       | xz -d -c \
       | sudo tar xf - -C /usr/local/bin/ --strip=1 shellcheck-stable/shellcheck
     sudo chown root:root /usr/local/bin/shellcheck
