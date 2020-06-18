55dd58c1c892a7d26255545acc248e560ce083e8
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2020-06-18 04:41:48 +0000

    Install a new git on Oracle Linux 7

--- scripts/install_git.sh
+++ scripts/install_git.sh
@@ -3,11 +3,23 @@ set -eu -o pipefail
 
 # shellcheck disable=SC1091
 readonly OS_ID=$(. /etc/os-release; echo "$ID")
+# shellcheck disable=SC1091
+readonly OS_VERSION=$(. /etc/os-release; echo "$VERSION")
 
 echo 'Install Git'
 case $OS_ID in
   ol)
-    sudo yum -y install git
+    case ${OS_VERSION%%.*} in
+      7)
+        sudo yum -y remove git
+        sudo yum -y localinstall https://repo.ius.io/ius-release-el7.rpm
+        sudo yum-config-manager --disable ius
+        sudo yum -y --enablerepo=ius install git222 
+        ;;
+      8)
+        sudo dnf -y install git
+        ;;
+    esac
     ;;
   ubuntu)
     sudo apt update
