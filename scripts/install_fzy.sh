94aae88aafa19123e1c16de8e6ec70b6d98f777d
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2020-06-19 11:51:24 +0900

    Avoid git's version conflict

--- scripts/install_fzy.sh
+++ scripts/install_fzy.sh
@@ -7,7 +7,7 @@ readonly OS_ID=$(. /etc/os-release; echo "$ID")
 echo 'Install fzy'
 case $OS_ID in
   ol)
-    sudo yum -y install gcc git make
+    sudo yum -y install gcc
     temp_dir=$(mktemp -d)
     pushd "$temp_dir"
     git clone https://github.com/jhawthorn/fzy
