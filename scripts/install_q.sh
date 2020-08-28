efa363464700808f057e6cc550f292b4e69feec3
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2020-08-28 15:19:32 +0900

    Avoid installing unstable versions

--- scripts/install_q.sh
+++ scripts/install_q.sh
@@ -2,11 +2,12 @@
 set -eu -o pipefail
 
 echo 'Install Q'
-readonly LATEST=$(
-  curl -sSI https://github.com/harelba/q/releases/latest \
-    | tr -d '\r' \
-    | awk -F'/' '/^location:/{print $NF}'
-)
+# readonly LATEST=$(
+#   curl -sSI https://github.com/harelba/q/releases/latest \
+#     | tr -d '\r' \
+#     | awk -F'/' '/^location:/{print $NF}'
+# )
+readonly LATEST=2.0.9 # 2.0.10 and 2.0.11 is not stable
 curl -L# "https://github.com/harelba/q/releases/download/${LATEST}/q-x86_64-Linux" \
   | sudo tee /usr/local/bin/q >/dev/null
 sudo chmod +x /usr/local/bin/q
