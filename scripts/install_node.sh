70a084e6e673e496afd52c1a30294eb35f03e346
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2020-12-01 18:51:49 +0900

    Bump Node.js from 12 to 14 and remove yarn

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -8,16 +8,11 @@ echo 'Install Node.js'
 # Check the latest version from https://nodejs.org/en/ and https://github.com/nodesource/distributions
 case $OS_ID in
   ol)
-    curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+    curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
     sudo yum -y install nodejs gcc-c++ make
-    curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo >/dev/null
-    sudo yum -y install yarn
     ;;
   ubuntu)
-    curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
     sudo apt -y install nodejs build-essential
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-    sudo apt -y install yarn
     ;;
 esac
