487dc8dbe905501b851d781de09a75ceddf4f13e
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-01-14 19:31:48 +0900

    Get the latest version from github

--- scripts/install_rubocop.sh
+++ scripts/install_rubocop.sh
@@ -2,6 +2,14 @@
 set -eu -o pipefail
 
 echo 'Install RuboCop'
-sudo bash "$(dirname "$0")/../bin/rubocop" -v
-sudo cp "$(dirname "$0")/../bin/rubocop" /usr/local/bin/rubocop
+readonly IMAGE_NAME='shakiyam/rubocop'
+if [[ $(command -v docker) ]]; then
+  sudo docker pull $IMAGE_NAME
+elif [[ $(command -v podman) ]]; then
+  podman pull $IMAGE_NAME
+else
+  echo -e "\033[36mdocker or podman not found\033[0m"; exit 1;
+fi
+curl -L# https://raw.githubusercontent.com/shakiyam/rubocop-docker/master/rubocop \
+  | sudo tee /usr/local/bin/rubocop >/dev/null
 sudo chmod +x /usr/local/bin/rubocop
