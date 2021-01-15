01087b16aac45b47904c9dcc066d4226658537bc
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-01-15 19:43:39 +0900

    Remove the code for podman because shakiyam/oci-cli does not work with podman

--- scripts/install_oci.sh
+++ scripts/install_oci.sh
@@ -3,13 +3,8 @@ set -eu -o pipefail
 
 echo 'Install OCI CLI'
 readonly IMAGE_NAME='shakiyam/oci-cli'
-if [[ $(command -v docker) ]]; then
-  sudo docker pull $IMAGE_NAME
-elif [[ $(command -v podman) ]]; then
-  podman pull $IMAGE_NAME
-else
-  echo -e "\033[36mdocker or podman not found\033[0m"; exit 1;
-fi
+[[ $(command -v docker) ]] || { echo -e "\033[36mdocker not found\033[0m"; exit 1; }
+sudo docker pull $IMAGE_NAME
 curl -L# https://raw.githubusercontent.com/shakiyam/oci-cli-docker/master/oci \
   | sudo tee /usr/local/bin/oci >/dev/null
 sudo chmod +x /usr/local/bin/oci
