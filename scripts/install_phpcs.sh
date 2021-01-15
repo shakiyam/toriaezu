6425695afb296ca39f5297894c1a11133bb4dcea
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-01-15 20:21:57 +0900

    Add confirmation of required command

--- scripts/install_phpcs.sh
+++ scripts/install_phpcs.sh
@@ -2,6 +2,13 @@
 set -eu -o pipefail
 
 echo 'Install PHP_CodeSniffer'
-sudo bash "$(dirname "$0")/../bin/phpcs" --version
+readonly IMAGE_NAME='phpqa/phpcs'
+if [[ $(command -v docker) ]]; then
+  sudo docker pull $IMAGE_NAME
+elif [[ $(command -v podman) ]]; then
+  podman pull $IMAGE_NAME
+else
+  echo -e "\033[36mdocker or podman not found\033[0m"; exit 1;
+fi
 sudo cp "$(dirname "$0")/../bin/phpcs" /usr/local/bin/phpcs
 sudo chmod +x /usr/local/bin/phpcs
