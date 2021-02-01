8f890858b2c134f35f8e2661f7fd3ac36b1f5c95
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-02-01 17:47:04 +0900

    Use the latest version for operating systems with the new glibc

--- scripts/install_docker-compose.sh
+++ scripts/install_docker-compose.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -eu -o pipefail
 
+# shellcheck disable=SC1091
+readonly OS_ID=$(. /etc/os-release; echo "$ID")
+# shellcheck disable=SC1091
+readonly OS_VERSION=$(. /etc/os-release; echo "$VERSION")
+
 echo 'Install Docker Compose'
-# readonly LATEST=$(
-#   curl -sSI https://github.com/docker/compose/releases/latest \
-#     | tr -d '\r' \
-#     | awk -F'/' '/^[Ll]ocation:/{print $NF}'
-# )
-readonly LATEST=1.27.4
+if [[ ${OS_ID:-} == 'ol' && ${OS_VERSION%%.*} -eq 7 ]]; then
+  readonly LATEST=1.27.4
+else
+  readonly LATEST=$(
+    curl -sSI https://github.com/docker/compose/releases/latest \
+      | tr -d '\r' \
+      | awk -F'/' '/^[Ll]ocation:/{print $NF}'
+  )
+fi
 curl -L# "https://github.com/docker/compose/releases/download/${LATEST}/docker-compose-$(uname -s)-$(uname -m)" \
   | sudo tee /usr/local/bin/docker-compose >/dev/null
 sudo chmod +x /usr/local/bin/docker-compose
--- scripts/install_hadolint.sh
+++ scripts/install_hadolint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -eu -o pipefail
 
+# shellcheck disable=SC1091
+readonly OS_ID=$(. /etc/os-release; echo "$ID")
+# shellcheck disable=SC1091
+readonly OS_VERSION=$(. /etc/os-release; echo "$VERSION")
+
 echo 'Install hadolint'
-# readonly LATEST=$(
-#   curl -sSI https://github.com/hadolint/hadolint/releases/latest \
-#     | tr -d '\r' \
-#     | awk -F'/' '/^[Ll]ocation:/{print $NF}'
-# )
-readonly LATEST=v1.19.0
+if [[ ${OS_ID:-} == 'ol' && ${OS_VERSION%%.*} -eq 7 ]]; then
+  readonly LATEST=v1.19.0
+else
+  readonly LATEST=$(
+    curl -sSI https://github.com/hadolint/hadolint/releases/latest \
+      | tr -d '\r' \
+      | awk -F'/' '/^[Ll]ocation:/{print $NF}'
+  )
+fi
 curl -L# "https://github.com/hadolint/hadolint/releases/download/${LATEST}/hadolint-Linux-x86_64" \
   | sudo tee /usr/local/bin/hadolint >/dev/null
 sudo chmod +x /usr/local/bin/hadolint
