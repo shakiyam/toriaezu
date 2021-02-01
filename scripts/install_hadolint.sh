289d8fce7c47e4c246842de9b5923dd73f606f9c
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-02-01 12:44:15 +0900

    Pin the version that does not require glibc 2.27

--- scripts/install_hadolint.sh
+++ scripts/install_hadolint.sh
@@ -2,11 +2,12 @@
 set -eu -o pipefail
 
 echo 'Install hadolint'
-readonly LATEST=$(
-  curl -sSI https://github.com/hadolint/hadolint/releases/latest \
-    | tr -d '\r' \
-    | awk -F'/' '/^[Ll]ocation:/{print $NF}'
-)
+# readonly LATEST=$(
+#   curl -sSI https://github.com/hadolint/hadolint/releases/latest \
+#     | tr -d '\r' \
+#     | awk -F'/' '/^[Ll]ocation:/{print $NF}'
+# )
+readonly LATEST=v1.19.0
 curl -L# "https://github.com/hadolint/hadolint/releases/download/${LATEST}/hadolint-Linux-x86_64" \
   | sudo tee /usr/local/bin/hadolint >/dev/null
 sudo chmod +x /usr/local/bin/hadolint
