04d392a7ac9ceaff46f2b213b689c661f13ae5fb
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-05-28 13:46:33 +0900

    Bump Go from 1.15.7 to 1.16.4

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -3,5 +3,5 @@ set -eu -o pipefail
 
 echo 'Install Go Programming Language'
 # Check the latest version from https://golang.org/dl/
-curl -L# https://golang.org/dl/go1.15.7.linux-amd64.tar.gz \
+curl -L# https://golang.org/dl/go1.16.4.linux-amd64.tar.gz \
   | sudo tar xzf - -C /usr/local
