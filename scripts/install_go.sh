0b406133bdd3b9ef0ef2b98dd586c913689e6574
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-05-28 13:51:41 +0900

    Support ARM architecture

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -3,5 +3,14 @@ set -eu -o pipefail
 
 echo 'Install Go Programming Language'
 # Check the latest version from https://golang.org/dl/
-curl -L# https://golang.org/dl/go1.16.4.linux-amd64.tar.gz \
+case $(uname -m) in
+  x86_64)
+    ARCHITECTURE=amd64
+    ;;
+  aarch64)
+    ARCHITECTURE=arm64
+    ;;
+esac
+readonly ARCHITECTURE
+curl -L# https://golang.org/dl/go1.16.4.linux-${ARCHITECTURE}.tar.gz \
   | sudo tar xzf - -C /usr/local
