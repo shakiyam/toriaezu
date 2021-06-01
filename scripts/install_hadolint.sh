355c95642fb8f4dea59a7294e49d6580b53d3e9e
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-06-01 09:32:34 +0900

    Use docker image for Oracle Linux 7

--- scripts/install_hadolint.sh
+++ scripts/install_hadolint.sh
@@ -8,15 +8,16 @@ OS_VERSION=$(. /etc/os-release; echo "$VERSION"); readonly OS_VERSION
 
 echo 'Install hadolint'
 if [[ ${OS_ID:-} == 'ol' && ${OS_VERSION%%.*} -eq 7 ]]; then
-  LATEST=v1.19.0
+  sudo docker pull hadolint/hadolint
+  sudo cp "$(dirname "$0")/../bin/hadolint" /usr/local/bin/hadolint
 else
   LATEST=$(
     curl -sSI https://github.com/hadolint/hadolint/releases/latest \
       | tr -d '\r' \
       | awk -F'/' '/^[Ll]ocation:/{print $NF}'
   )
+  readonly LATEST
+  curl -L# "https://github.com/hadolint/hadolint/releases/download/${LATEST}/hadolint-Linux-x86_64" \
+    | sudo tee /usr/local/bin/hadolint >/dev/null
 fi
-readonly LATEST
-curl -L# "https://github.com/hadolint/hadolint/releases/download/${LATEST}/hadolint-Linux-x86_64" \
-  | sudo tee /usr/local/bin/hadolint >/dev/null
 sudo chmod +x /usr/local/bin/hadolint
