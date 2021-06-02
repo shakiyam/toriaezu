5b24a6a14cbe3c42ccef0d83af22551c3eb41b33
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-06-02 15:15:38 +0900

    Use docker image

--- scripts/install_docker-compose.sh
+++ scripts/install_docker-compose.sh
@@ -7,21 +7,15 @@ OS_ID=$(. /etc/os-release; echo "$ID"); readonly OS_ID
 OS_VERSION=$(. /etc/os-release; echo "$VERSION"); readonly OS_VERSION
 
 echo 'Install Docker Compose'
+sudo docker pull ghcr.io/linuxserver/docker-compose:latest
+curl -L# https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh \
+  | sudo tee /usr/local/bin/docker-compose >/dev/null
+sudo chmod +x /usr/local/bin/docker-compose
 LATEST=$(
   curl -sSI https://github.com/docker/compose/releases/latest \
     | tr -d '\r' \
     | awk -F'/' '/^[Ll]ocation:/{print $NF}'
 )
 readonly LATEST
-if [[ ${OS_ID:-} == 'ol' && ${OS_VERSION%%.*} -eq 7 ]]; then
-  readonly DOCKER_COMPOSE_IMAGE_TAG='ghcr.io/linuxserver/docker-compose:latest'
-  sudo docker pull $DOCKER_COMPOSE_IMAGE_TAG
-  curl -L# https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh \
-    | sudo tee /usr/local/bin/docker-compose >/dev/null
-else
-  curl -L# "https://github.com/docker/compose/releases/download/${LATEST}/docker-compose-$(uname -s)-$(uname -m)" \
-    | sudo tee /usr/local/bin/docker-compose >/dev/null
-fi
-sudo chmod +x /usr/local/bin/docker-compose
 curl -L# "https://raw.githubusercontent.com/docker/compose/${LATEST}/contrib/completion/bash/docker-compose" \
   | sudo tee /etc/bash_completion.d/docker-compose >/dev/null
