0a030dea41a8d145cb77c014dfdb7789113b6b15
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-06-02 12:24:02 +0900

    Fix omission of condition check

--- scripts/install_rg.sh
+++ scripts/install_rg.sh
@@ -5,12 +5,12 @@ set -eu -o pipefail
 OS_ID=$(. /etc/os-release; echo "$ID"); readonly OS_ID
 
 echo 'Install ripgrep'
-if [[ $(uname -m) == 'aarch64' ]]; then
-  echo 'ripgrep is not yet available as an ARM binary.'
-  exit 0
-fi
 case $OS_ID in
   ol)
+    if [[ $(uname -m) == 'aarch64' ]]; then
+      echo 'ripgrep is not yet available as an ARM binary.'
+      exit 0
+    fi
     sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
     sudo yum -y install ripgrep
     ;;
