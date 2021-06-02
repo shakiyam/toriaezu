858c4fdd11c6d5bc7810e3adff120a43f840c175
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-06-02 11:29:26 +0900

    Support ARM architecture

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -23,8 +23,17 @@ case $OS_ID in
     sudo apt -y install --no-install-recommends \
       apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    case $(uname -m) in
+      x86_64)
+        ARCHITECTURE=amd64
+        ;;
+      aarch64)
+        ARCHITECTURE=arm64
+        ;;
+    esac
+    readonly ARCHITECTURE
     sudo add-apt-repository \
-      "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+      "deb [arch=$ARCHITECTURE] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt update
     sudo apt -y install docker-ce
     ;;
