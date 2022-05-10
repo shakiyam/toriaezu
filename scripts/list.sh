47148cbd7d526b1073b726c1b3f92b1e26a6d98a
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2022-05-10 10:29:58 +0900

    Use Docker Compose V2 except podman

--- scripts/list.sh
+++ scripts/list.sh
@@ -10,8 +10,16 @@ echo 'Installed Software:'
 
 show_version 'cho'                       1 'cho -v'
 show_version 'csvq'                      1 'csvq -v'
-show_version 'Docker'                    1 "$(command -v podman || command -v docker) -v"
-show_version 'Docker Compose'            1 'docker-compose version'
+if [[ $(command -v docker) ]]; then
+  show_version 'Docker'                  1 'docker -v'
+else
+  show_version 'Docker'                  1 'podman -v'
+fi
+if [[ $(command -v docker-compose) ]]; then
+  show_version 'Docker Compose'          1 'docker-compose -v'
+else
+  show_version 'Docker Compose'          1 'docker compose version'
+fi
 show_version 'dockviz'                   1 'dockviz -v'
 show_version 'fzy'                       1 'fzy -v'
 show_version 'Git'                       1 'git --version'
