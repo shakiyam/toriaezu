0054b6dc0d784992bf6bad53ac4a37fe16bca39a
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2022-08-19 16:18:18 +0900

    Use Java 17 on Oracle Linux 8 and 9

--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -7,11 +7,27 @@ OS_ID=$(
   echo "$ID"
 )
 readonly OS_ID
+# shellcheck disable=SC1091
+OS_VERSION=$(
+  . /etc/os-release
+  echo "$VERSION"
+)
+readonly OS_VERSION
 
 echo 'Install OpenJDK Development Kit'
 case $OS_ID in
   ol)
-    sudo yum -y install java-11-openjdk-devel
+    case ${OS_VERSION%%.*} in
+      7)
+        sudo yum -y install java-11-openjdk-devel
+        ;;
+      8)
+        sudo dnf -y install java-17-openjdk-devel
+        ;;
+      9)
+        sudo dnf -y install java-17-openjdk-devel
+        ;;
+    esac
     ;;
   ubuntu)
     sudo apt update
