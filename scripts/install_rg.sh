2d873e2f010b29224668612c86f10523ea61df9d
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2023-02-22 16:05:46 +0900

    Use EPEL

--- scripts/install_rg.sh
+++ scripts/install_rg.sh
@@ -17,22 +17,24 @@ readonly OS_VERSION
 echo 'Install ripgrep'
 case $OS_ID in
   ol)
-    if [[ $(uname -m) == 'aarch64' ]]; then
-      echo 'ripgrep is not yet available as an ARM binary.'
-      exit 0
-    fi
     case ${OS_VERSION%%.*} in
       7)
-        sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
-        sudo yum -y install ripgrep
+        case $(uname -m) in
+          aarch64)
+            echo 'ripgrep Arm binary for Oracle Linux 7 is not available.'
+            exit 0
+            ;;
+          x86_64)
+            sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+            sudo yum -y install ripgrep
+            ;;
+        esac
         ;;
       8)
-        sudo dnf config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
-        sudo dnf -y install ripgrep
+        sudo dnf -y --enablerepo=ol8_developer_EPEL install ripgrep
         ;;
       9)
-        echo 'ripgrep is not yet supported on Oracle Linux 9.'
-        exit 0
+        sudo dnf -y --enablerepo=ol9_developer_EPEL install ripgrep
         ;;
     esac
     ;;
