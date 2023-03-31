e5ee305450c3a528d1c93ae98efc8bbbfa06d57e
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2023-03-31 12:31:15 +0900

    Avoid glibc version dependencies

--- scripts/install_csvq.sh
+++ scripts/install_csvq.sh
@@ -8,16 +8,4 @@ LATEST=$(
     | awk -F'/' '/^[Ll]ocation:/{print $NF}'
 )
 readonly LATEST
-case $(uname -m) in
-  x86_64)
-    ARCHITECTURE=amd64
-    ;;
-  aarch64)
-    ARCHITECTURE=arm
-    ;;
-esac
-readonly ARCHITECTURE
-curl -L# "https://github.com/mithrandie/csvq/releases/download/${LATEST}/csvq-${LATEST}-linux-${ARCHITECTURE}.tar.gz" \
-  | sudo tar xzf - -C /usr/local/bin/ --strip=1 "csvq-${LATEST}-linux-${ARCHITECTURE}/csvq"
-sudo chown root:root /usr/local/bin/csvq
-sudo chmod 755 /usr/local/bin/csvq
+/usr/local/go/bin/go install "github.com/mithrandie/csvq@$LATEST"
