c70ad1bfe045f5d86bee99f812c35c393f907aaf
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-01-22 01:50:47 +0000

    Change branch name

--- scripts/install_oci.sh
+++ scripts/install_oci.sh
@@ -6,7 +6,7 @@ DOCKER=$(command -v docker || command -v podman) || {
   echo -e "\033[36mdocker or podman not found\033[0m"
   exit 1
 }
-curl -L# https://raw.githubusercontent.com/shakiyam/oci-cli-docker/master/oci \
+curl -L# https://raw.githubusercontent.com/shakiyam/oci-cli-docker/main/oci \
   | sudo tee /usr/local/bin/oci >/dev/null
 sudo chmod +x /usr/local/bin/oci
 sudo -u "$(id -un)" "$DOCKER" pull ghcr.io/shakiyam/oci-cli
