39c298a97b1ffb912a73bba148909ce46deea4df
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-02-28 02:07:45 +0000

    Use the released binaries

--- scripts/install_cho.sh
+++ scripts/install_cho.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 set -eu -o pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-readonly SCRIPT_DIR
-
 echo 'Install cho'
 case $(uname -m) in
   x86_64)
-    LATEST=$(
-      curl -sSI https://github.com/mattn/cho/releases/latest \
-        | tr -d '\r' \
-        | awk -F'/' '/^[Ll]ocation:/{print $NF}'
-    )
-    readonly LATEST
-    curl -L# "https://github.com/mattn/cho/releases/download/${LATEST}/cho_${LATEST}_linux_amd64.tar.gz" \
-      | sudo tar xzf - -C /usr/local/bin/ --strip=1 "cho_${LATEST}_linux_amd64/cho"
+    ARCHITECTURE=amd64
     ;;
   aarch64)
-    "$SCRIPT_DIR/install_go.sh"
-    /usr/local/go/bin/go install github.com/mattn/cho@latest
+    ARCHITECTURE=arm64
     ;;
 esac
+readonly ARCHITECTURE
+LATEST=$(
+  curl -sSI https://github.com/mattn/cho/releases/latest \
+    | tr -d '\r' \
+    | awk -F'/' '/^[Ll]ocation:/{print $NF}'
+)
+readonly LATEST
+curl -L# "https://github.com/mattn/cho/releases/download/${LATEST}/cho_${LATEST}_linux_${ARCHITECTURE}.tar.gz" \
+  | sudo tar xzf - -C /usr/local/bin/ --strip=1 "cho_${LATEST}_linux_${ARCHITECTURE}/cho"
