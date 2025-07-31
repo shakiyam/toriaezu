1b3b4188a6324be2ee35b9ee083a2e352c81b596
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-07-31 05:57:49 +0000

    Refactor Docker installation script to improve consistency and security

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -15,13 +15,10 @@ case $OS_ID in
     install_package podman podman-plugins
     ;;
   ubuntu)
-    sudo apt-get update
-    sudo DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends \
-      ca-certificates curl gnupg lsb-release
+    install_package gnupg
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
-      | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+      | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
     case $(uname -m) in
       x86_64)
         ARCHITECTURE=amd64
@@ -34,20 +31,24 @@ case $OS_ID in
     echo \
       "deb [arch=$ARCHITECTURE signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
         | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-    sudo apt-get update
+    sudo chmod 644 /etc/apt/keyrings/docker.gpg /etc/apt/sources.list.d/docker.list
     install_package docker-ce
     ;;
 esac
 
 echo 'Setup Docker Engine'
-if [[ ${OS_ID:-} == 'ol' ]]; then
-  sudo -u "$(id -un)" XDG_RUNTIME_DIR=/run/user/"$(id -u)" systemctl --user daemon-reload
-  sudo -u "$(id -un)" XDG_RUNTIME_DIR=/run/user/"$(id -u)" systemctl --user enable --now podman.socket
-else
-  sudo systemctl daemon-reload
-  sudo systemctl restart docker
-  sudo systemctl enable docker
-  LOGNAME="${SUDO_USER:-$(logname 2>/dev/null || id -nu)}"
-  readonly LOGNAME
-  sudo usermod -aG docker "$LOGNAME"
-fi
+LOGNAME="${SUDO_USER:-$(logname 2>/dev/null || id -nu)}"
+readonly LOGNAME
+case $OS_ID in
+  ol)
+    sudo -u "$LOGNAME" XDG_RUNTIME_DIR=/run/user/"$(id -u "$LOGNAME")" systemctl --user daemon-reload
+    sudo -u "$LOGNAME" XDG_RUNTIME_DIR=/run/user/"$(id -u "$LOGNAME")" systemctl --user enable podman.socket
+    sudo -u "$LOGNAME" XDG_RUNTIME_DIR=/run/user/"$(id -u "$LOGNAME")" systemctl --user restart podman.socket
+    ;;
+  ubuntu)
+    sudo systemctl daemon-reload
+    sudo systemctl enable docker
+    sudo systemctl restart docker
+    sudo usermod -aG docker "$LOGNAME"
+    ;;
+esac
