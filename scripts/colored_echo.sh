89c185508ef14b1c9ca097530670dbe1ced40754
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-08-01 05:03:20 +0000

    Replace tput commands with ANSI escape sequences to eliminate ncurses dependency

--- scripts/colored_echo.sh
+++ scripts/colored_echo.sh
@@ -3,42 +3,32 @@ set -eu -o pipefail
 
 echo_error() {
   if [[ -t 1 ]]; then
-    tput setaf 1 # Red
-    tput bold
-  fi
-  echo "$@"
-  if [[ -t 1 ]]; then
-    tput sgr0
+    echo -e "\033[1;31m$*\033[0m" # Bold Red
+  else
+    echo "$@"
   fi
 }
 
 echo_warn() {
   if [[ -t 1 ]]; then
-    tput setaf 3 # Yellow
-    tput bold
-  fi
-  echo "$@"
-  if [[ -t 1 ]]; then
-    tput sgr0
+    echo -e "\033[1;33m$*\033[0m" # Bold Yellow
+  else
+    echo "$@"
   fi
 }
 
 echo_info() {
   if [[ -t 1 ]]; then
-    tput setaf 6 # Cyan
-  fi
-  echo "$@"
-  if [[ -t 1 ]]; then
-    tput sgr0
+    echo -e "\033[36m$*\033[0m" # Cyan
+  else
+    echo "$@"
   fi
 }
 
 echo_success() {
   if [[ -t 1 ]]; then
-    tput setaf 2 # Green
-  fi
-  echo "$@"
-  if [[ -t 1 ]]; then
-    tput sgr0
+    echo -e "\033[32m$*\033[0m" # Green
+  else
+    echo "$@"
   fi
 }
