516401f3ff801c8536ca04f7c7e68986b089decc
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-08-01 09:03:36 +0000

    Change dtools installation path to follow XDG Base Directory specification

--- scripts/install_dtools.sh
+++ scripts/install_dtools.sh
@@ -8,6 +8,6 @@ BIN_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/../bin" && pwd)
 readonly BIN_DIR
 
 echo_info 'Install dtools'
-mkdir -p "$HOME/bin"
-cp -v "$BIN_DIR/dcls" "$HOME/bin/"
-cp -v "$BIN_DIR/dclogs" "$HOME/bin/"
+mkdir -p "$HOME/.local/bin"
+cp -v "$BIN_DIR/dcls" "$HOME/.local/bin/"
+cp -v "$BIN_DIR/dclogs" "$HOME/.local/bin/"
