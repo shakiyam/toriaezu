74fb062ece98184dd71b3443dbbebb0fd92ac5d7
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-09-18 08:46:33 +0000

    Remove dotfile modifications from mise installer to support chezmoi-managed configs

--- scripts/install_mise.sh
+++ scripts/install_mise.sh
@@ -8,23 +8,5 @@ echo_info 'Install mise'
 curl -fsSL --proto '=https' --tlsv1.2 https://mise.jdx.dev/install.sh | sh
 eval "$(mise activate bash)"
 
-if [ -f "$HOME/.bashrc" ]; then
-  if ! grep -q 'mise activate bash' "$HOME/.bashrc" 2>/dev/null; then
-    # shellcheck disable=SC2016
-    echo 'eval "$(mise activate bash)"' >>"$HOME/.bashrc"
-    echo_info 'Added mise activation to ~/.bashrc'
-  fi
-fi
-
-if command -v fish &>/dev/null; then
-  FISH_CONFIG="$HOME/.config/fish/config.fish"
-  if [ -f "$FISH_CONFIG" ]; then
-    if ! grep -q 'mise activate fish' "$FISH_CONFIG" 2>/dev/null; then
-      echo 'mise activate fish | source' >>"$FISH_CONFIG"
-      echo_info 'Added mise activation to Fish config'
-    fi
-  fi
-fi
-
 echo_info 'Verify mise installation'
 verify_installation mise
